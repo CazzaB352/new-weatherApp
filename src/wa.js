a5cc22805a3e4410a322eaa17142c839c7c9fa7b
CazzaB352 <99604109+CazzaB352@users.noreply.github.com>
2022-05-22 16:58:57 +0100

    increased font size for forecast

--- src/wa.js
+++ src/wa.js
@@ -109,6 +109,7 @@ function search(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
+
 function searchLocation(position) {
   let apiKey = "5779c89c46453cf0e47b584677d15c00";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
