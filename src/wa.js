abe9f7bfa069cbbdec2ac41c16e575b65f60c458
CazzaB352 <99604109+CazzaB352@users.noreply.github.com>
2022-05-22 15:40:22 +0100

    adjusting columns for forecast
    more width
    Changed miles per hour - wind

--- src/wa.js
+++ src/wa.js
@@ -48,7 +48,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-              <div class="col-2">
+              <div class="col-md" "dayCol">
                 <div class="weather-forecast-date">${formatDay(
                   forecastDay.dt
                 )}</div>
