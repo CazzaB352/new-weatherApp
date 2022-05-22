557749a96bee7592107e852eeba3956a5bec8c4b
CazzaB352 <99604109+CazzaB352@users.noreply.github.com>
2022-05-22 14:15:44 +0100

    testing to ensure can get forecast results

--- src/wa.js
+++ src/wa.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -54,7 +55,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   let apiKey = "5779c89c46453cf0e47b584677d15c00";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -123,5 +124,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-displayForecast();
